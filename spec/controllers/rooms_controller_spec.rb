e54bc9ca43dc4bfb61776d1459c80da5a0c16597
Naoya1625 <n_s2_2080@yahoo.co.jp>
2021-01-23 11:40:01 +0900

    finish home.html.erb and responsive-design

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe RoomsController, type: :controller do
       # 正常にレスポンスを返すこと 
       it "responds successfully" do
         sign_in @user
-        get :my_room
+        get :posts
         expect(response).to be_successful
       end
       # 200レスポンスを返すこと
       it "returns a 200 response" do
         sign_in @user
-        get :my_room
+        get :posts
         expect(response).to have_http_status "200"
       end
     end
@@ -26,12 +26,12 @@ RSpec.describe RoomsController, type: :controller do
     context "as a guest" do
       # 302レスポンスを返すこと
       it "returns a 302 response" do
-        get :my_room
+        get :posts
         expect(response).to have_http_status "302"
       end
       # サインイン画面にリダイレクトすること 
       it "redirects to the sign-in page" do
-        get :my_room
+        get :posts
         expect(response).to redirect_to "/users/sign_in"
       end
     end
